7affdf54dd070d073e636cf89a848a8877b37c64
conner-brew <89354650+conner-brew@users.noreply.github.com>
2022-11-27 14:17:21 -0500

    testing va/ca toggle

--- streamlitva/main.py
+++ streamlitva/main.py
@@ -28,7 +28,7 @@ state = st.selectbox('Select which state you want to explore:',['Virginia','Cali
 
 st.info('NOTE: This tool is not meant to be used in decision-making for specific cases. This sandbox can only show the impact that a feature may present, absent any context. Risk of abuse is a complex issue, and is strongly affected by the unique attributes of the caretaker family, environment, and the case itself. ')
 
-while state == 'Virgina':
+if st.button('Virginia'):
     st.markdown('In the state of Virginia, the following ten variables have been shown in modeling to be the most important features determining the likelihood of abuse in a foster case. Experiment with the features below, then press the "Predict Risk" button to see how it might affect risk!')
 
     Housing = st.radio('Is the child living in housing that always meets standards of care?', ['Yes','No'])
@@ -167,9 +167,9 @@ while state == 'Virgina':
             for i in neg:
                 st.markdown("- " + i)
 
-while state == 'California':
+if st.button('California'):
      st.header('CALI')
-     st.markdown('In the state of Virginia, the following ten variables have been shown in modeling to be the most important features determining the likelihood of abuse in a foster case. Experiment with the features below, then press the "Predict Risk" button to see how it might affect risk!')
+     st.markdown('In the state of California, the following ten variables have been shown in modeling to be the most important features determining the likelihood of abuse in a foster case. Experiment with the features below, then press the "Predict Risk" button to see how it might affect risk!')
 
      Housing = st.radio('Is the child living in housing that always meets standards of care?', ['Yes','No'])
      if Housing == 'Yes':
